8074df0745f87b3244d801f4cc923e30ca553222
fchb1239 <fchb1239.dk@gmail.com>
2021-10-10 12:51:23 +0200

    Add files via upload

--- Quest/buildQMOD.ps1
+++ Quest/buildQMOD.ps1
@@ -10,5 +10,5 @@ $ArchiveName = "nodrag.qmod"
 $TempArchiveName = "nodrag.qmod.zip"
 
 & $buildScript NDK_PROJECT_PATH=$PSScriptRoot APP_BUILD_SCRIPT=$PSScriptRoot/Android.mk NDK_APPLICATION_MK=$PSScriptRoot/Application.mk
-Compress-Archive -Path "./libs/arm64-v8a/libnodrag.so", "./libs/arm64-v8a/libbeatsaber-hook_3_1_2.so", "./extern/libcustom-types.so", "./mod.json", "cover.png" -DestinationPath $TempArchiveName -Force
+Compress-Archive -Path "./libs/arm64-v8a/libnodrag.so", "./libs/arm64-v8a/libbeatsaber-hook_3_1_2.so", "./mod.json", "cover.png" -DestinationPath $TempArchiveName -Force
 Move-Item $TempArchiveName $ArchiveName -Force
