e26f413263f4276f3bfc51e142f90b5ff77afa6f
fchb1239 <fchb1239.dk@gmail.com>
2021-10-19 13:57:06 +0200

    Add files via upload

--- Quest/src/main.cpp
+++ Quest/src/main.cpp
@@ -2,6 +2,8 @@
 #include "modloader/shared/modloader.hpp"
 #include "beatsaber-hook/shared/utils/hooking.hpp"
 #include "beatsaber-hook/shared/utils/il2cpp-utils.hpp"
+#include <stdlib.h>
+#include <string>
 
 static ModInfo modInfo;
 
@@ -15,10 +17,38 @@ Logger &getLogger()
     return *logger;
 }
 
-MAKE_HOOK_FIND_CLASS_UNSAFE_INSTANCE(FlightController_Start, "", "FlightController", "Start", void, Il2CppObject* self)
+std::string toLower(std::string string) {
+    for (int i = 0; i < string.length(); i++)
+      {
+          string[i] = tolower(string[i]);
+      }
+    return string;
+}
+
+bool modWork = false;
+
+MAKE_HOOK_FIND_CLASS_UNSAFE_INSTANCE(GameManager_OnPrivateOKButtonPressed, "", "GameManager", "OnPrivateOKButtonPressed", void, Il2CppObject* self)
 {
-    FlightController_Start(self);
-    CRASH_UNLESS(il2cpp_utils::SetFieldValue(self, "dragFactor", 0.0f));
+    GameManager_OnPrivateOKButtonPressed(self);
+    Il2CppString* privName = CRASH_UNLESS(il2cpp_utils::GetFieldValue<Il2CppString*>(self, "privateServerName"));
+    std::string roomName = toLower(to_utf8(csstrtostr(privName)));
+    std::string allowedString = "mod";
+    if (roomName.find(allowedString) != std::string::npos || roomName.find(allowedString) != std::string::npos) {
+        modWork = true;
+    }
+    if (roomName.find(allowedString) == std::string::npos && roomName.find(allowedString) == std::string::npos) {
+        modWork = false;
+    }
+}
+
+MAKE_HOOK_FIND_CLASS_UNSAFE_INSTANCE(FlightController_FixedUpdate, "", "FlightController", "FixedUpdate", void, Il2CppObject* self)
+{
+    FlightController_FixedUpdate(self);
+    if(modWork){
+        CRASH_UNLESS(il2cpp_utils::SetFieldValue(self, "dragFactor", 0.0f));
+    }else{
+        CRASH_UNLESS(il2cpp_utils::SetFieldValue(self, "dragFactor", 10.0f));
+    }
 }
 
 extern "C" void setup(ModInfo &info)
@@ -34,7 +64,8 @@ extern "C" void load()
     Logger& logger = getLogger();
     logger.info("Installing hooks...");
     
-    INSTALL_HOOK(getLogger(), FlightController_Start);
+    INSTALL_HOOK(getLogger(), FlightController_FixedUpdate);
+    INSTALL_HOOK(getLogger(), GameManager_OnPrivateOKButtonPressed);
 
     logger.info("Installed all hooks!");
 }
