fff1a853e103cafe0914cf546b92e5005f03ae1d
fchb1239 <fchb1239.dk@gmail.com>
2021-10-19 13:57:41 +0200

    Add files via upload

--- Quest/src/main.cpp
+++ Quest/src/main.cpp
@@ -27,7 +27,7 @@ std::string toLower(std::string string) {
 
 bool modWork = false;
 
-MAKE_HOOK_FIND_CLASS_UNSAFE_INSTANCE(GameManager_OnPrivateOKButtonPressed, "", "GameManager", "OnPrivateOKButtonPressed", void, Il2CppObject* self)
+MAKE_HOOK_FIND_CLASS_UNSAFE_INSTANCE(GameManager_OnPrivateOKButtonPressed, "", "GameManager", "OnPrivateOKButtonPressed", void, Il2CppObject* self) //thanks wolta
 {
     GameManager_OnPrivateOKButtonPressed(self);
     Il2CppString* privName = CRASH_UNLESS(il2cpp_utils::GetFieldValue<Il2CppString*>(self, "privateServerName"));
